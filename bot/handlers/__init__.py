4fa17781b31f529bce7a273c0a1b3be67139d738
ErmVolga <fhrisnov@vk.com>
2025-03-12 02:40:46 +0300

    Создание комнат и подключение к ним, добавление роутера из game.py, добавление инлайн-клавиатуры для начала игры, чуть изменил подключение роутеров

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,2 +1,6 @@
 from .commands import router as commands_router  # роутер команд
 from .admin import router as admin_router  # роутер админ панели
+from .game import router as game_router  # Подключаем роутер из game.py
+
+# Экспортируем все роутеры
+__all__ = ["commands_router", "admin_router", "game_router"]
